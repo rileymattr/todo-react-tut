b95c0069f2d8f2620c94388bb2702ca4e1e99b3b
Matthew Riley <rileymattr@gmail.com>
2023-05-12 12:26:17 -0700

    Complete Mozilla react tutorial.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { nanoid } from "nanoid";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+//tutorial here: https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_interactivity_events_state#state_and_the_usestate_hook
 
-function App() {
+const FILTER_MAP = {
+  All: () => true,
+  Complete: (task) => task.completed === true,
+  Active: (task) => task.completed === false,
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+function App(props) {
+  function addTask(name) {
+    const newTask = {id: `task-${nanoid()}`, name, completed: false};
+    setTasks([...tasks, newTask]);
+  }
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if(task.id === id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const updatedTasks = tasks.filter((task) => 
+      id !== task.id
+    );
+    setTasks(updatedTasks);
+  }
+
+  function editTask(id, newName) {
+    const updatedTasks = tasks.map((task) => {
+      if(task.id === id) {
+        return { ...task, name: newName};
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState("All");
+
+  const filterList = FILTER_NAMES.map((name) =>
+    <FilterButton 
+      key={name} 
+      name={name} 
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  );
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed} 
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+  ));
+
+  const taskPlural = taskList.length === 1 ? "task" : "tasks";
+  const headingText = `${taskList.length} ${taskPlural} remaining`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask}/>
+      <div className="filters btn-group stack-exception">
+        {
+        filterList
+        }
+      </div>
+      <h2 id="list-heading">{headingText}</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
